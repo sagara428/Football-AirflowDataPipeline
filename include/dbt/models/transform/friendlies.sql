fffd3c6b91b1f97d6a71f1471d3f0bfdf34971c6
Topas Sagara <tsagara28@gmail.com>
2023-11-05 15:51:23 +0700

    update friendlies table model

--- include/dbt/models/transform/friendlies.sql
+++ include/dbt/models/transform/friendlies.sql
@@ -22,9 +22,7 @@ SELECT
          ELSE 'Draw'
     END AS result,
     t1.team_id AS home_team_id,
-    t2.team_id AS away_team_id,
-    dt.tournament_id AS tournament_id
+    t2.team_id AS away_team_id
 FROM friendlies AS f
 JOIN {{ ref('team_stats') }} AS t1 ON f.home_team = t1.team_name
 JOIN {{ ref('team_stats') }} AS t2 ON f.away_team = t2.team_name
-JOIN {{ ref('tournament_eras') }} AS dt ON f.tournament = dt.tournament
